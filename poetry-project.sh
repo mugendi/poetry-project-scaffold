364177f0bde7b691b8dfd85bd3402748b6f02b82
Anthony Mugendi <ngurumugz@gmail.com>
2023-06-21 09:47:08 +0300

    Added references note

--- poetry-project.sh
+++ poetry-project.sh
@@ -284,6 +284,7 @@ function emoji() {
         tick | t) emoji="✔" ;;
         cross) emoji="✗" ;;
         palette | p) emoji="🎨" ;;
+        book | b) emoji="📖" ;;
         *) emoji="" ;;
 
         esac
@@ -643,6 +644,16 @@ work_in_project_dir() {
                 see $(c Bu)https://squidfunk.github.io/mkdocs-material/reference/$(c 0Yi)         
          - You can also edit $(c Ws)'docs/styles/extra.css'$(c 0Yi) for additional css
          " "palette"
+    echo
+
+    echo
+    note "Check out these references:
+         - pyproject.toml configs: $(c Bu)https://github.com/python-poetry/poetry/blob/master/pyproject.toml$(c 0Yi)
+         - Pypi classifiers: $(c Bu)https://gist.github.com/nazrulworld/3800c84e28dc464b2b30cec8bc1287fc$(c 0Yi)
+         - mkdocs: $(c Bu)https://mkdocs-dupe-test.readthedocs.io/$(c 0Yi)
+         - Docs theme: $(c Bu)https://squidfunk.github.io/mkdocs-material/reference/$(c 0Yi) 
+         - Also checkout documentations for $(c Ws)flake8, isort & black$(c 0)
+         " "book"
     echo
 
 }
